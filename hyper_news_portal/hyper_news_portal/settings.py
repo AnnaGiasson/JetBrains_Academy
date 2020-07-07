c3b940dde4b31e55262774e20780f4b13e0b5457
Anna Giasson <AnnaGraceGiasson@gmail.com>
2020-07-07 13:29:08 -0400

    - Updated gitignore to include vscode temporary files - Began Django project "hyper_news_portal", file structure in place,   currently import errors prevent the server from running

--- hyper_news_portal/hyper_news_portal/settings.py
+++ hyper_news_portal/hyper_news_portal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'g)kof#j)fbh41bg(t)$12$vv++fy$)&dhoqyz@!%bpezam&##n'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -73,13 +74,13 @@ WSGI_APPLICATION = 'hyper_news_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+DATABASES = {}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
